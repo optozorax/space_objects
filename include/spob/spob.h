5470f70d88f0c0ae6e566af3a320023f82ec55ac
Sheprut Ilya <optitel223@gmail.com>
2019-01-23 00:20:01 +0700

    Добавлено преобразование координат для массивов
    Сделано это при помощи функций toMas, toDirMas, fromMas. Это поддерживается для всех классов.

--- include/spob/spob.h
+++ include/spob/spob.h
@@ -1,7 +1,26 @@
 #pragma once
 
+#include <vector>
+
 #include <spob/vector2.h>
 #include <spob/vector3.h>
 #include <spob/coordinates2.h>
 #include <spob/coordinates3.h>
 #include <spob/intersection.h>
+
+//-----------------------------------------------------------------------------
+// Функции для массового преобразования
+#define mas_func(a) \
+template<class C, class T> \
+auto a##Mas(const C& c, const std::vector<T>& mas) { \
+	std::vector<decltype(c.a##(mas.front()))> result; \
+	result.reserve(mas.size()); \
+	for (const auto& i : mas) \
+		result.push_back(c.a##(i)); \
+	return result; \
+}
+
+mas_func(to)
+mas_func(toDir)
+mas_func(from)
+mas_func(fromDir)
