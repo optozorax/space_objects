cbbc658dfaadf3660ab2df81a82e5b3dc98db164
Sheprut Ilya <optitel223@gmail.com>
2019-02-02 19:15:06 +0700

    Рефакторинг FindBorders

--- doc/find_borders.cpp
+++ doc/find_borders.cpp
@@ -4,14 +4,16 @@ namespace spob
 {
 
 //-----------------------------------------------------------------------------
-FindBorders::FindBorders(int maxSize, int border) : m_maxSize(maxSize), m_border(border), space2(getStandardCrd2()) {
+
+//-----------------------------------------------------------------------------
+BorderFinder2::BorderFinder2() {
 	double inf = std::numeric_limits<double>::infinity();
 	min = vec2(inf, inf);
 	max = vec2(-inf, -inf);
 }
 
 //-----------------------------------------------------------------------------
-void FindBorders::process(vec2 point) {
+void BorderFinder2::process(const vec2& point) {
 	if (point.x > max.x) max.x = point.x;
 	if (point.y > max.y) max.y = point.y;
 	if (point.x < min.x) min.x = point.x;
@@ -19,13 +21,27 @@ void FindBorders::process(vec2 point) {
 }
 
 //-----------------------------------------------------------------------------
-void FindBorders::process(const std::vector<vec2>& points) {
+void BorderFinder2::process(const std::vector<vec2>& points) {
 	for (const auto& i : points)
 		process(i);
 }
 
 //-----------------------------------------------------------------------------
-void FindBorders::finish(void) {
+bool BorderFinder2::isInside(vec2 pos) const {
+	return (pos.x > min.x) && (pos.y > min.y) && (pos.x < max.x) && (pos.y < max.y);
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+BorderFinderForImage::BorderFinderForImage(int maxSize, int border, bool isDimensionsMustBeEqual, bool isYMustBeFlipped) : m_maxSize(maxSize), m_border(border), space2(getStandardCrd2()), m_isDimensionsMustBeEqual(isDimensionsMustBeEqual), m_isYMustBeFlipped(isYMustBeFlipped) {
+}
+
+//-----------------------------------------------------------------------------
+void BorderFinderForImage::finish(void) {
+	// Считаем смещение и масштаб
 	if (max.x - min.x > max.y - min.y) {
 		m_size.x = m_maxSize;
 		m_size.y = (max.y - min.y)/(max.x - min.x) * m_size.x;
@@ -40,41 +56,39 @@ void FindBorders::finish(void) {
 	m_offset = -min;
 	m_size += vec2(m_border) * 2;
 
+	// Задаем положение и оси в соответствии с этим
 	i /= m_scale;
 	j /= m_scale;
 	pos = min - m_border * (i + j);
 
+	// Инвертируем систему координат
 	auto crd = spob::invert(*this);
 	i = crd.i;
 	j = crd.j;
 	pos = crd.pos;
+
+	// Если задан необходимый флаг, то смещаем систему координат и делаем размеры изображения одинаковыми по обоим осям
+	if (m_isDimensionsMustBeEqual) {
+		pos += (vec2(m_maxSize + m_border) - m_size)/2.0;
+		m_size = vec2(m_maxSize + m_border);
+	}
+
+	// Если ось Y должна быть перевернута, то делаем это
+	/* Без инвертирования считается, что экранная система координат начинается в левом верхнем углу. С инвертированием считается, что экранная система координат начинается в левом нижнем углу, что более похоже на математический вариант. */
+	if (m_isYMustBeFlipped) {
+		space2 standard_corner = getStandardCrd2();
+		standard_corner.pos = vec2(0, m_size.y);
+		standard_corner.j.negate();
+		auto crd = standard_corner.to(*this);
+		i = crd.i;
+		j = crd.j;
+		pos = crd.pos;
+	}
 }
 
 //-----------------------------------------------------------------------------
-void FindBorders::invertY(void) {
-	space2 standard_corner = getStandardCrd2();
-	standard_corner.pos = vec2(0, m_size.y);
-	standard_corner.j.negate();
-	auto crd = standard_corner.to(*this);
-	i = crd.i;
-	j = crd.j;
-	pos = crd.pos;
-}
-
-//-----------------------------------------------------------------------------
-vec2 FindBorders::getCalculatedSize(void) const {
+vec2 BorderFinderForImage::getCalculatedSize(void) const {
 	return m_size;
-}
-
-//-----------------------------------------------------------------------------
-void FindBorders::offsetToEqualSize(void) {
-	pos += (vec2(m_maxSize + m_border) - m_size)/2.0;
-	m_size = vec2(m_maxSize + m_border);
-}
-
-//-----------------------------------------------------------------------------
-bool FindBorders::isInside(vec2 pos) const {
-	return (pos.x > min.x) && (pos.y > min.y) && (pos.x < max.x) && (pos.y < max.y);
 }
 
 };
--- doc/find_borders.h
+++ doc/find_borders.h
@@ -7,39 +7,44 @@ namespace spob
 {
 
 	//-------------------------------------------------------------------------
-	/** Класс для нахождения ограничивающего прямоугольника для точек, а так же, позволяющий далее эти точки нарисовать на изображении с максимальным размером maxSize и с заданным отступом от краев изображения border. */
-	class FindBorders : public space2
+	/** Класс для нахождения ограничивающего прямоугольника для точек. */
+	class BorderFinder2
 	{
 	public:
-		FindBorders(int maxSize, int border);
+		BorderFinder();
 
-		/** Сюда передается точка для обработки. */
 		void process(vec2 point);
-
-		/** Сюда передается массив точек для обработки. */
 		void process(const std::vector<vec2>& points);
 
+		bool isInside(vec2 pos) const;
+
+		vec2 min, max;
+	};
+
+	//-------------------------------------------------------------------------
+	/** Класс для преобразования координат из тех, что даны, к координатам изображения. Обладатем теми же методами и свойствами, что и классы, от которых он наследуется. */
+	class BorderFinderForImage : public space2, public BorderFinder2
+	{
+	public:
+		BorderFinderForImage(int maxSize, int border, bool isDimensionsMustBeEqual = false, bool isYMustBeFlipped = true);
+		
 		/** После обработки всех точек обязательно надо завершить обработку этой функцией, потому что там высчитываются необходимые значения. */
 		void finish(void);
 
 		/** Возвращает итоговый размер изображения. */
 		vec2 getCalculatedSize(void) const;
 
-		void offsetToEqualSize(void);
-
-		/** Определяет, находится ли pos внутри рассчитанных границ. */
-		bool isInside(vec2 pos) const;
-
-		/** Если раньше считалось, что пиксели начинаются с левого верхнего угла, то после этой функции они будут считаться с левого нижнего угла. */
-		void invertY(void);
-
-		/** Прямой доступ к переменным min и max. */
-		vec2 min, max;
+		/** 
+			Метод to() преобразует координаты из передаваемых в process к координатам изображения.
+			Метод from() преобразует координаты из координат изображения к координатам, которые передавались в process.
+		*/
 	private:
 		int m_maxSize, m_border;
 		double m_scale;
 		vec2 m_offset;
 		vec2 m_size;
+		bool m_isDimensionsMustBeEqual;
+		bool m_isYMustBeFlipped;
 	};
 
 };
